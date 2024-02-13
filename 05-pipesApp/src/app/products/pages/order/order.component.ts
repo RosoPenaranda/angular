c6a5c02d49d661cf7e612883b06ed1425f0884cc
Roso Peñaranda <roso.penaranda@payrix.com>
2024-02-13 12:04:21 -0500

    finished custom pipes section

--- 05-pipesApp/src/app/products/pages/order/order.component.ts
+++ 05-pipesApp/src/app/products/pages/order/order.component.ts
@@ -1,5 +1,5 @@
-import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { Color, Hero } from '../../interfaces/hero.interface';
 
 @Component({
   selector: 'products-order',
@@ -7,8 +7,41 @@ import { Component } from '@angular/core';
 })
 export class OrderComponent {
   public isUpperCase: boolean = false;
+  public orderBy: keyof Hero | '' = 'canFly';
+
+  public heroes: Hero[] = [
+    {
+      name: 'Superman',
+      canFly: true,
+      color: Color.blue,
+    },
+    {
+      name: 'Batman',
+      canFly: false,
+      color: Color.black,
+    },
+    {
+      name: 'Robin',
+      canFly: false,
+      color: Color.red,
+    },
+    {
+      name: 'Flash',
+      canFly: false,
+      color: Color.red,
+    },
+    {
+      name: 'Green Lantern',
+      canFly: true,
+      color: Color.green,
+    },
+  ];
 
   toggleUperCase(): void {
     this.isUpperCase = !this.isUpperCase;
+  }
+
+  changeOrder(value: string): void {
+    this.orderBy = value as keyof Hero;
   }
 }
