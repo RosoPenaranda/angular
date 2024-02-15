66caabf7c68c237c344ad49aecf18c65ad3e3572
Roso Peñaranda <rosopenaranda@gmail.com>
2024-02-14 20:49:49 -0500

    started heroesApp

---
+++ 06-heroesApp/src/app/auth/pages/layout-page/layout-page.component.ts
@@ -0,0 +1,8 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'auth-layout-page',
+  templateUrl: './layout-page.component.html',
+})
+export class LayoutPageComponent {}
