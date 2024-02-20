9555ed844f17f0d2b32858212549d5b374c1e6be
Roso Peñaranda <roso.penaranda@payrix.com>
2024-02-20 16:33:02 -0500

    heroesApp wip

--- 06-heroesApp/src/app/auth/auth.module.ts
+++ 06-heroesApp/src/app/auth/auth.module.ts
@@ -4,6 +4,7 @@ import { AuthRoutingModule } from './auth-routing.module';
 import { LayoutPageComponent } from './pages/layout-page/layout-page.component';
 import { LoginPageComponent } from './pages/login-page/login-page.component';
 import { RegisterPageComponent } from './pages/register-page/register-page.component';
+import { MaterialModule } from '../material/material.module';
 
 const components = [];
 
@@ -13,6 +14,6 @@ const components = [];
     LoginPageComponent,
     RegisterPageComponent,
   ],
-  imports: [CommonModule, AuthRoutingModule],
+  imports: [CommonModule, AuthRoutingModule, MaterialModule],
 })
 export class AuthModule {}
