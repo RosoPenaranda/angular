1c50828d98dfb9f2eef6c9ef3b0517505d354c95
Roso Peñaranda <roso.penaranda@payrix.com>
2024-02-29 13:50:02 -0500

    wip heroesApp

--- 06-heroesApp/src/app/app.module.ts
+++ 06-heroesApp/src/app/app.module.ts
@@ -1,17 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SharedModule } from './shared/shared.module';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, SharedModule],
-  bootstrap: [AppComponent],
-  providers: [
-    provideAnimationsAsync()
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+    AppRoutingModule,
+    SharedModule,
   ],
+  bootstrap: [AppComponent],
+  providers: [provideAnimationsAsync()],
 })
 export class AppModule {}
---
+++ 06-heroesApp/src/app/heroes/services/heroes.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Hero } from '../interfaces/hero.interface';
+import { Observable, catchError, of } from 'rxjs';
+import { environment } from '../../../environments/environments';
+
+@Injectable({ providedIn: 'root' })
+export class HeroesService {
+  private baseUrl: string = environment.baseURL;
+  constructor(private http: HttpClient) {}
+
+  getHeroes(): Observable<Hero[]> {
+    return this.http.get<Hero[]>(`${this.baseUrl}/heroes`);
+  }
+
+  getHeroById(id: string): Observable<Hero | undefined> {
+    return this.http
+      .get<Hero>(`${this.baseUrl}/heroes/${id}`)
+      .pipe(catchError((error) => of(undefined)));
+  }
+}
